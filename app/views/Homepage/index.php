93e83a3f9475c23f0a3c64606caa32762191252c
Thomas Braakman 336469 <336469@student.mboutrecht.nl>
2023-03-14 12:13:01 +0100

    update1

---
+++ app/views/Homepage/index.php
@@ -0,0 +1,3 @@
+<h3><?= $data['title']; ?></h3>
+<a href="http://mvc-oop-toets.com/zangeres/index">index</a><br>
+<a href="http://mvc-oop-toets.com/zangeres/getzangeres">top 5 rijkste zangeressen ter wereld</a>
